fd6eb92e6526053b11b1042dd84f7c240139c6fd
David Reinfurt <reinfurt@o-r-g.com>
2016-12-02 13:01:22 -0500

    clean up for submit pkg to guggenheim

--- mobile/index.php
+++
@@ -1,9 +0,0 @@
-<?
-    require_once("../views/head.php");
-    require_once("../views/video.php");
-    require_once("../views/foot.php");
-?>
-
-
-
-
--- mobile/processing/index.php
+++
@@ -1,9 +0,0 @@
-<?
-    require_once("../../views/head.php");
-    require_once("../../views/processing.php");
-    require_once("../../views/foot.php");
-?>
-
-
-
-
