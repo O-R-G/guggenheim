ee7c7e1d1d5103439931424e7ebfa35243f02365
David Reinfurt <reinfurt@o-r-g.com>
2016-12-06 09:03:38 -0500

    add cloudfonts and $host for guggenheim.org -- revised submittal

--- views/head.php
+++ views/head.php
@@ -1,6 +1,7 @@
 <? 
-$host = "http://dev.o-r-g.local/guggenheim";
+// $host = "http://dev.o-r-g.local/guggenheim";
 // $host = "http://dev.o-r-g.com/guggenheim";
+$host = "http://www.guggenheim.org/holiday";
 ?>
 
 <!DOCTYPE html>
@@ -11,6 +12,9 @@ $host = "http://dev.o-r-g.local/guggenheim";
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 		<link rel='stylesheet' type='text/css' media='all' href='<? echo $host ?>/static/css/main.css'>
    		<link rel="shortcut icon" type="image/png" href="/media/png/icon.png"/>
+        <!-- dev.o-r-g.com -->
 		<!-- <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/710324/6123172/css/fonts.css"/> -->
+        <!-- www.guggenheim.org/holiday -->
+		<link rel="stylesheet" type="text/css" href="https://cloud.typography.com/710324/624620/css/fonts.css"/>
 	</head>
 	<body>
