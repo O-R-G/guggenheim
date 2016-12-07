d443895028f13e96bfc27810e8282d3547608287
david reinfurt <reinfurt@ussava.pair.com>
2016-12-07 17:47:17 -0500

    adjust css

--- views/head.php
+++ views/head.php
@@ -1,6 +1,6 @@
 <? 
-$host = "http://dev.o-r-g.local/guggenheim";
-// $host = "http://dev.o-r-g.com/guggenheim";
+// $host = "http://dev.o-r-g.local/guggenheim";
+$host = "http://dev.o-r-g.com/guggenheim";
 // $host = "http://web.guggenheim.org/holiday";
 ?>
 
@@ -12,10 +12,20 @@ $host = "http://dev.o-r-g.local/guggenheim";
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 		<link rel='stylesheet' type='text/css' media='all' href='<? echo $host ?>/static/css/main.css'>
    		<link rel="shortcut icon" type="image/png" href="/media/png/icon.png"/>
-        <!-- dev.o-r-g.com -->
-		<!-- <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/710324/6123172/css/fonts.css"/> -->
-        <!-- www.guggenheim.org/holiday -->
+	        <!-- dev.o-r-g.com -->
+		<link rel="stylesheet" type="text/css" href="https://cloud.typography.com/710324/6123172/css/fonts.css"/>
+        	<!-- www.guggenheim.org/holiday -->
 		<!-- <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/710324/624620/css/fonts.css"/> -->
+		<!-- Google Analytics -->
+		<!-- 
+		<script>
+		window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
+		ga('create', 'UA-5676651-2', 'auto');
+		ga('send', 'pageview');
+		</script>
+		<script async src='https://www.google-analytics.com/analytics.js'></script> 
+		-->
+		<!-- End Google Analytics -->
 	</head>
 	<body>
     <div id="logo">
