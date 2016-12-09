e70d18826dd3f391a9c1139fa945dad664462d16
David Reinfurt <reinfurt@o-r-g.com>
2016-12-08 21:24:34 -0500

    ibid

--- views/head.php
+++ views/head.php
@@ -11,11 +11,10 @@ $host = "http://web.guggenheim.org/holiday";
 		<meta charset="utf-8">
 		<meta name="viewport" content="width=device-width, user-scalable=no">
 		<link rel='stylesheet' type='text/css' media='all' href='<? echo $host ?>/static/css/main.css'>
-   		<link rel="shortcut icon" type="image/png" href="/media/png/icon.png"/>
         <!-- dev.o-r-g.com -->
 		<!-- <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/710324/6123172/css/fonts.css"/> -->
         <!-- www.guggenheim.org/holiday -->
-		<link rel="stylesheet" type="text/css" href="https://cloud.typography.com/710324/624620/css/fonts.css"/>
+		<!-- <link rel="stylesheet" type="text/css" href="https://cloud.typography.com/710324/624620/css/fonts.css"/> -->
         <!-- Google Analytics -->
 		<script>
 		window.ga=window.ga||function(){(ga.q=ga.q||[]).push(arguments)};ga.l=+new Date;
